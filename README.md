c09a734bd47a5a0a243855004d10a326d8167eda
milosz263 <millosz263@outlook.com>
2023-01-19 16:10:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Simple console application that attempts to fix The Sims 2 Ultimate Collection registry entries that were broken after using cleaning software.
+
+### Requierments: .NET Framework 4.8
+### USAGE:
+After launching (requires admin privileges) just click "Yes" in Registy Editor dialog.
+
---
+++ Sims2RegFix/RegBuilder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace Sims2RegFix
+{
+    internal static class RegBuilder
+    {
+        private static string filePath = "";
+
+        private const string valEnd = "\\Fun with Pets\\SP9\\TSBin\\Sims2EP9.exe";
+        public static void GenerateRegFile()
+        {
+
+            var reg = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\Sims2EP9.exe", false);
+            string exePath = reg.GetValue("").ToString();
+            if (File.Exists(exePath))
+            {
+                Console.WriteLine("Detected game exe: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0}", exePath);
+                Console.ResetColor();
+
+                string templatePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                if (!templatePath.EndsWith("\\"))
+                    templatePath += "\\";
+
+                string gamePath = exePath.Substring(0, exePath.Length - valEnd.Length);
+
+                Console.WriteLine("Game installation path: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0}", gamePath);
+                Console.ResetColor();
+
+                string template = File.ReadAllText(templatePath + "Sims2paths.dat");
+                template.Replace("{GamePath}", gamePath);
+                filePath = templatePath + "Sims2pathFix.reg";
+
+                if (File.Exists(filePath))
+                    File.Delete(filePath);
+                File.WriteAllText(filePath, template, System.Text.Encoding.Unicode);
+
+                Console.WriteLine("Finished generation of *.reg file: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0}", filePath);
+                Console.ResetColor();
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR: Could not detect game location. File: {0} does not exist.", exePath);
+                Console.ResetColor();
+            }
+        }
+
+        public static void ExecuteRegFile()
+        {
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("Calling RegEdit...");
+                Process reg = Process.Start("regedit.exe", "\"" + filePath + "\"");
+                reg.WaitForExit();
+            }
+        }
+        public static string WriteRegFile()
+        {
+            if (File.Exists(filePath))
+            {
+                return File.ReadAllText(filePath);
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
